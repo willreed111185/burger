20ede2feeb734a8ca6247c160c1bdeb0f87dc3bd
willreed111185 <william.reed.11@gmail.com>
2017-10-30 10:49:47 -0400

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
+var orm = require("../config/orm.js");
 
+var burger = {
+  selectAll: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(val, cb) {
+    orm.create("burgers", "burger_name", val, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(burger_name, cb) {
+    orm.update("burgers", burger_name, 1, function(res) {
+      cb(res);
+    });
+  }
+};
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
