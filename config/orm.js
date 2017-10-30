db41d2d62a96961dc7fc2cc8bb937badc2aff330
willreed111185 <william.reed.11@gmail.com>
2017-10-30 11:10:40 -0400

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,29 @@
-var connection = require("./connection.js");
-var orm = {  
-  selectAll: function(tableInput) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result) {
-      console.log(result);
-    });
-  },
-  insertOne: function(tableInput, inputName, inputValue) {
-    var queryString = "INSERT INTO ?? (??) VALUES (?)";
-    connection.query(queryString, [tableInput,inputName,inputValue], function(err, result) {
-      console.log(result);
-    });
-  },
-  updateOne: function(tableInput, colName, colValue, condName, condValue) {
-    var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-    connection.query(queryString, [tableInput, colName, colValue, condName, condValue], function(err, result) {
-      console.log(result);
-    });
-  },
-};
+var connection = require('./connection.js');
+var orm = {
+  
+	selectAll: function(cb){
+		var queryString = "SELECT * FROM burgers";
+		connection.query(queryString, function(err, result) {
+			if (err) {throw err;}
+			cb(result);
+		});
+	},
+  
+	insertOne: function(val, cb){
+		var queryString = "INSERT INTO burgers (burger_name) VALUE (?)";
+		connection.query(queryString, [val], function(err, result){
+			if(err){throw err;}
+			cb(result);
+		});
+	},
+  
+	updateOne: function(val, cb){
+		var queryString = "UPDATE burgers SET devoured=true WHERE id=?";
+		connection.query(queryString, [val], function(err, result){
+			if(err){throw err;}
+			cb(result);
+		});
+    
+	}
+}
 module.exports = orm;
