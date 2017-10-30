3143953351800492c6bd0a1e41a1449e5915e07b
willreed111185 <william.reed.11@gmail.com>
2017-10-30 10:31:05 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
+### Schema
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+  devoured boolean default false
+	PRIMARY KEY (id)
+);
