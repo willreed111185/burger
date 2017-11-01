d3228c1d711cfa8896e48a6bda1ab13177c7a895
willreed111185 <william.reed.11@gmail.com>
2017-11-01 09:20:10 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 var connection;
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+  connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
