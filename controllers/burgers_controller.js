75259914778accee44b38227ccdc271c1ceeeebb
willreed111185 <william.reed.11@gmail.com>
2017-11-01 08:50:26 -0400

    make it better

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,9 @@ router.post('/', function(req, res){
 });
 
 router.put('/api/burgers/:id', function(req, res){
+	console.log(req.params.id);
 	burger.updateOne(req.params.id, function(result){
+
 		if (result.changedRows == 0) {
 			return res.status(404).end();
 		} else {
